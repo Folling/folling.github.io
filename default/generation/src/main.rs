f243e04376985e36dcfed057083fa3d2454bcc88
Folling <coding@folling.de>
2022-03-20 19:07:59 +0100

    Generate root sites properly from sites-tree

--- default/generation/src/main.rs
+++ default/generation/src/main.rs
@@ -1,7 +1,18 @@
-use std::io::prelude::*;
-use std::path::Path;
+use std::fmt::Write as _;
+use std::io::Write as _;
+use std::path::{Path, PathBuf};
 
+use crate::util::tree::ImmutableTree;
 use anyhow::*;
+
+pub mod content;
+pub mod util;
+
+#[macro_use]
+extern crate log;
+extern crate simplelog;
+
+use simplelog::*;
 
 pub fn capitalise(s: &str) -> String {
     let mut c = s.chars();
@@ -12,40 +23,86 @@ pub fn capitalise(s: &str) -> String {
 }
 
 fn main() -> Result<()> {
+    TermLogger::init(
+        LevelFilter::Info,
+        ConfigBuilder::new().clear_filter_ignore().build(),
+        TerminalMode::Mixed,
+        ColorChoice::Auto,
+    )?;
+
     let content = Path::new("../content");
 
     if !content.exists() {
-        bail!("invalid path to content: {}", std::fs::canonicalize(content)?.display());
+        bail!("invalid path to content: {}", content.canonicalize()?.display());
     }
 
-    println!("content path is {:?}", std::fs::canonicalize(content));
+    info!("content path is {:?}", content.canonicalize());
 
     if !content.is_dir() {
         bail!("content path isn't a directory");
     }
 
+    let out = Path::new("../out");
+
+    info!("Out path is: {:?}", out.canonicalize()?.display());
+
     let sites_folder = content.join("sites");
 
     if !sites_folder.exists() {
+        bail!("sites folder didn't exist in content: {}", sites_folder.canonicalize()?.display());
+    }
+
+    if !sites_folder.is_dir() {
         bail!(
-            "sites folder didn't exist in content: {}",
-            std::fs::canonicalize(sites_folder)?.display()
+            "sites folder exists but isn't a directory: {}",
+            sites_folder.canonicalize()?.display()
         );
     }
 
-    let meta_str = concat!(
-        "<!DOCTYPE html>\n",
-        "<html>\n",
-        "<head>\n",
-        "    <title>Rewrites Are Bliss</title>\n",
-        "    <meta content=\"width=device-width, initial-scale=1.0\" name=\"viewport\">\n",
-        "    <link rel=\"stylesheet\" type=\"text/css\" href=\"../../../styles/default.css\"/>\n",
-        "    <link rel=\"stylesheet\" href=\"../../../styles/codetheme.css\">\n",
-        "    <script src=\"https://kit.fontawesome.com/d7fa8be03e.js\" crossorigin=\"anonymous\"></script>\n",
-        "    <script src=\"../../../vendor/highlight/highlight.min.js\"></script>\n",
-        "    <script src=\"../../../scripts/main.js\"></script>\n",
-        "</head>\n"
-    );
+    info!("sites folder found at {}", sites_folder.canonicalize()?.display());
+
+    info!("generating sites tree");
+
+    let mut sites_tree = ImmutableTree::<PathBuf>::new(std::fs::read_dir(sites_folder)?.filter_map(|v| v.map(|v| v.path()).ok()));
+
+    sites_tree.add_layers_recursively(|path| {
+        if !path.is_dir() {
+            return None;
+        }
+
+        if let std::result::Result::Ok(entry) = std::fs::read_dir(path) {
+            Some(entry.filter_map(|v| v.ok().map(|v| v.path()).filter(|v| v.is_dir())))
+        } else {
+            None
+        }
+    });
+
+    if sites_tree.is_empty() {
+        bail!("site tree is empty");
+    }
+
+    info!("sites tree is generated as:\n{:?}", sites_tree);
+
+    info!("Generating html string");
+
+    let meta_str = |depth| {
+        format!(
+            concat!(
+                "<!DOCTYPE html>\n",
+                "<html>\n",
+                "<head>\n",
+                "    <title>Rewrites Are Bliss</title>\n",
+                "    <meta content=\"width=device-width, initial-scale=1.0\" name=\"viewport\">\n",
+                "    <link rel=\"stylesheet\" type=\"text/css\" href=\"{dots}styles/default.css\"/>\n",
+                "    <link rel=\"stylesheet\" href=\"{dots}styles/codetheme.css\">\n",
+                "    <script src=\"https://kit.fontawesome.com/d7fa8be03e.js\" crossorigin=\"anonymous\"></script>\n",
+                "    <script src=\"{dots}vendor/highlight/highlight.min.js\"></script>\n",
+                "    <script src=\"{dots}scripts/main.js\"></script>\n",
+                "</head>\n"
+            ),
+            dots = "../".repeat(depth + 4)
+        )
+    };
 
     let pre_header = concat!(
         "<body>\n",
@@ -54,6 +111,30 @@ fn main() -> Result<()> {
         "            <p id=\"header_title_label\">Rewrites Are Bliss</p>\n",
         "            <div id=\"header_container\">\n"
     );
+
+    let mut header = String::with_capacity(8192);
+
+    for i in 0..sites_tree.root_count() {
+        let root = sites_tree
+            .get(i)
+            .ok_or(anyhow!("sites tree contained fewer items than root_count"))?
+            .val();
+
+        write!(
+            header,
+            concat!(
+                "            <div class=\"tl_menu_item\">\n",
+                "               <p>{}</p>\n",
+                "            </div>\n"
+            ),
+            capitalise(
+                root.file_name()
+                    .map(|v| v.to_str())
+                    .flatten()
+                    .ok_or(anyhow!("unable to obtain file name from site: {}", root.canonicalize()?.display()))?
+            )
+        )?;
+    }
 
     let post_header = concat!(
         "            </div>\n",
@@ -77,36 +158,46 @@ fn main() -> Result<()> {
 
     let definitions = content.join("definitions");
 
-    let sites = std::fs::read_dir(sites_folder)?
-        .into_iter()
-        .map(|v| v.map(|v| v.path()))
-        .collect::<Result<Vec<_>, _>>()?;
+    info!("generating out directory");
 
-    let main_sites_str = sites
-        .iter()
-        .map(|v| {
-            Ok(format!(
-                concat!(
-                    "            <div class=\"tl_menu_item\">\n",
-                    "               <p>{}</p>\n",
-                    "            </div>\n"
-                ),
-                v.file_name()
-                    .map(|v| v.to_str().map(|v| capitalise(v)))
-                    .flatten()
-                    .ok_or(anyhow!("cannot obtain filename for site {:?}", v))?
-            ))
-        })
-        .collect::<Result<String, _>>()?;
+    if out.exists() {
+        info!("out directory already exists, removing previous installation");
+        std::fs::remove_dir_all(out)?;
+    }
 
-    for path in sites {
-        let mut index_html = std::fs::File::create(path.join("index.html"))?;
-        index_html.write_all(meta_str.as_bytes())?;
-        index_html.write_all(pre_header.as_bytes())?;
-        index_html.write_all(main_sites_str.as_bytes())?;
-        index_html.write_all(post_header.as_bytes())?;
-        index_html.write_all(pre_content.as_bytes())?;
-        index_html.write_all(post_content.as_bytes())?;
+    info!("creating out directory");
+
+    std::fs::create_dir(out)?;
+
+    info!("generating HTML files");
+
+    for site in sites_tree.iter() {
+        let site_value = site.val();
+
+        info!(
+            "generating {}",
+            site_value.file_name().map(|v| v.to_str()).flatten().ok_or(anyhow!(
+                "unable to obtain file name from {:?}",
+                site_value.canonicalize()?.display()
+            ))?
+        );
+
+        let joined = out.join(site_value.strip_prefix("..").unwrap());
+
+        if site_value.is_dir() {
+            std::fs::create_dir_all(&joined).unwrap();
+
+            let mut index_html = std::fs::File::create(joined.join("index.html"))?;
+
+            index_html.write_all(meta_str(site.layer()).as_bytes())?;
+            index_html.write_all(pre_header.as_bytes())?;
+            index_html.write_all(header.as_bytes())?;
+            index_html.write_all(post_header.as_bytes())?;
+            index_html.write_all(pre_content.as_bytes())?;
+            index_html.write_all(post_content.as_bytes())?;
+        } else {
+            bail!("non-directory found in file-tree at {}", site_value.canonicalize()?.display());
+        }
     }
 
     Ok(())
---
+++ default/generation/src/util/tree.rs
@@ -0,0 +1,250 @@
+use std::fmt::{Debug, Display, Formatter};
+
+#[derive(Clone, Ord, PartialOrd, Eq, PartialEq, Hash)]
+pub struct ImmutableTree<T> {
+    items: Vec<ImmutableTreeNode<T>>,
+    root_count: usize,
+}
+
+#[derive(Copy, Clone, Debug, Ord, PartialOrd, Eq, PartialEq, Hash)]
+pub struct ImmutableTreeNode<T> {
+    val: T,
+    idx: usize,
+    layer: usize,
+    parent: Option<usize>,
+    children_anchors: Option<(usize, usize)>,
+}
+
+impl<T> ImmutableTree<T> {
+    pub fn new<I>(root_iter: I) -> ImmutableTree<T>
+    where
+        I: IntoIterator<Item = T>,
+    {
+        let items = root_iter
+            .into_iter()
+            .enumerate()
+            .map(|(i, val)| ImmutableTreeNode {
+                val,
+                idx: i,
+                layer: 0,
+                parent: None,
+                children_anchors: None,
+            })
+            .collect::<Vec<_>>();
+
+        let root_count = items.len();
+
+        ImmutableTree { items, root_count }
+    }
+
+    pub fn add_layer<F, I>(&mut self, iter_gen: F)
+    where
+        F: Fn(&T) -> Option<I>,
+        I: IntoIterator<Item = T>,
+    {
+        if self.items.is_empty() {
+            return;
+        }
+
+        let mut idx = self.items.len();
+        let last_item = self.items.last().unwrap();
+        let last_layer = last_item.layer;
+        let new_layer = last_layer + 1;
+
+        let mut new_items = Vec::with_capacity(2_u32.pow(self.items.last().unwrap().layer as u32) as usize);
+
+        for item in self.items.iter_mut().skip_while(|p| p.layer != last_layer) {
+            let start = idx;
+
+            if let Some(iter) = iter_gen(&item.val) {
+                new_items.extend(iter.into_iter().enumerate().map(|(_, val)| {
+                    let ret = ImmutableTreeNode {
+                        val,
+                        idx,
+                        layer: new_layer,
+                        parent: Some(item.idx),
+                        children_anchors: None,
+                    };
+
+                    idx += 1;
+
+                    ret
+                }));
+
+                item.children_anchors = Some((start, idx));
+            }
+        }
+
+        self.items.append(&mut new_items);
+    }
+
+    pub fn add_layers_recursively<F, I>(&mut self, iter_gen: F)
+    where
+        F: Fn(&T) -> Option<I>,
+        I: IntoIterator<Item = T>,
+    {
+        if self.items.is_empty() {
+            return;
+        }
+
+        loop {
+            let size = self.len();
+
+            self.add_layer(&iter_gen);
+
+            if self.len() == size {
+                break;
+            }
+        }
+    }
+
+    pub fn len(&self) -> usize {
+        self.items.len()
+    }
+
+    pub fn root_count(&self) -> usize {
+        self.root_count
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.items.is_empty()
+    }
+
+    pub fn iter(&self) -> std::slice::Iter<ImmutableTreeNode<T>> {
+        self.items.iter()
+    }
+
+    pub fn iter_mut(&mut self) -> std::slice::IterMut<ImmutableTreeNode<T>> {
+        self.items.iter_mut()
+    }
+
+    pub fn get(&self, idx: usize) -> Option<&ImmutableTreeNode<T>> {
+        self.items.get(idx)
+    }
+}
+
+impl<T> ImmutableTree<T> {
+    pub fn print<F: Clone + Fn(&mut Formatter<'_>, &T) -> core::fmt::Result>(
+        &self,
+        f: &mut Formatter<'_>,
+        print_func: F,
+    ) -> core::fmt::Result {
+        if self.items.is_empty() {
+            write!(f, "━━root\n")?;
+        }
+
+        write!(f, "root\n")?;
+
+        for item in self.items.iter().take_while(|v| v.parent.is_none()) {
+            self.print_item(f, item, print_func.clone(), 1)?;
+        }
+
+        Ok(())
+    }
+
+    pub fn print_item<F: Clone + Fn(&mut Formatter<'_>, &T) -> core::fmt::Result>(
+        &self,
+        f: &mut Formatter<'_>,
+        item: &ImmutableTreeNode<T>,
+        print_func: F,
+        indent: usize,
+    ) -> core::fmt::Result {
+        write!(
+            f,
+            "{}{}━",
+            {
+                let mut ret = String::new();
+
+                if let Some(parent) = item.parent {
+                    let mut curr = &self.items[parent];
+
+                    while let Some(parent) = curr.parent {
+                        let idx = curr.idx;
+                        curr = &self.items[parent];
+                        if idx != curr.children_anchors.unwrap().1 - 1 {
+                            ret.insert_str(0, "┃ ")
+                        } else {
+                            ret.insert_str(0, "  ")
+                        }
+                    }
+
+                    if curr.idx != self.root_count - 1 {
+                        ret.insert_str(0, "┃ ")
+                    } else {
+                        ret.insert_str(0, "  ")
+                    }
+                }
+
+                ret
+            },
+            if let Some(parent) = item.parent {
+                if item.idx == self.items[parent].children_anchors.unwrap().1 - 1 {
+                    "┗"
+                } else {
+                    "┣"
+                }
+            } else {
+                if item.idx != self.root_count - 1 {
+                    "┣"
+                } else {
+                    "┗"
+                }
+            }
+        )?;
+
+        print_func(f, &item.val)?;
+
+        write!(f, "\n")?;
+
+        if let Some(anchors) = item.children_anchors {
+            for i in anchors.0..anchors.1 {
+                self.print_item(f, &self.items[i], print_func.clone(), indent + 1)?;
+            }
+        }
+
+        Ok(())
+    }
+}
+
+impl<T: Debug> Debug for ImmutableTree<T> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        self.print(f, |f, v| write!(f, "{:?}", v))
+    }
+}
+
+impl<T: Display> Display for ImmutableTree<T> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        self.print(f, |f, v| write!(f, "{}", v))
+    }
+}
+
+impl<T> IntoIterator for ImmutableTree<T> {
+    type Item = ImmutableTreeNode<T>;
+    type IntoIter = std::vec::IntoIter<Self::Item>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.items.into_iter()
+    }
+}
+
+impl<T> ImmutableTreeNode<T> {
+    pub fn val(&self) -> &T {
+        &self.val
+    }
+
+    pub fn idx(&self) -> usize {
+        self.idx
+    }
+
+    pub fn layer(&self) -> usize {
+        self.layer
+    }
+
+    pub fn parent(&self) -> Option<usize> {
+        self.parent
+    }
+
+    pub fn children_anchors(&self) -> Option<(usize, usize)> {
+        self.children_anchors
+    }
+}
